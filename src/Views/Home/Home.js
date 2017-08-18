4132cdb85c98d5d16a5d451a296df0911f013418
asarnaout <asarnaout@hotmail.com>
2017-08-18 14:38:27 +0200

    Added a link to the sign up page in the home component

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './Home.css';
 import '../../Assets/css/common.css';
 import InputText from '../../Components/InputText'
 import Button from '../../Components/Button'
-
 
 class HomeComponent extends Component {
 
     render() {
         return (
         <div id="HomeContainer" className="col-xs-offset-4 col-xs-4">
-            <h1 className="black-text center-text no-margin">Craving for Beer?</h1>
+            <h1 className="black-text center-text no-margin">Craving Beer?</h1>
           
             <div className="h10"/>
             <div className="h10"/>
@@ -41,7 +41,7 @@ class HomeComponent extends Component {
 
             <div className="row">
                 <div className="col-xs-10 col-xs-offset-1">
-                    <span className="fs-18">Not a member? Join Now</span>
+                    <span className="fs-18">Not a member? <Link to={'/signup'}>Join Now</Link></span>
                 </div>
             </div>
 
