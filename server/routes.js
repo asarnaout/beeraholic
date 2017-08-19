1246df763a09f6d8c33c37ad3757e760b552899a
asarnaout <asarnaout@hotmail.com>
2017-08-19 16:24:50 +0200

    Moving route handling to a separate file and installing the CORS module

---
+++ server/routes.js
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const path = require('path');
+const config = require('./config.js');
+
+function handleRoutes(express) {
+    const app = express();
+    app.use(cors());
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.use("/static/media/", express.static(__dirname + '/../build/static/media'));
+    app.use("/static/css/", express.static(__dirname + '/../build/static/css'));
+    app.use("/static/js/", express.static(__dirname + '/../build/static/js'));
+    app.use("/service-worker.js", express.static(__dirname + '/../build'));
+    app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '/../build', 'index.html')); });
+    app.listen(config.port, () => console.log('listening to connections on port: ' + config.port));
+}
+
+module.exports = handleRoutes;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const path = require('path');
-const app = express();
 const config = require('./config.js');
+const handleRoutes = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.use("/static/media/", express.static(__dirname + '/../build/static/media'));
-app.use("/static/css/", express.static(__dirname + '/../build/static/css'));
-app.use("/static/js/", express.static(__dirname + '/../build/static/js'));
-app.use("/service-worker.js", express.static(__dirname + '/../build'));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../build', 'index.html'));
-  });
-
-app.listen(config.port, () => console.log('listening to connections on port: ' + config.port));
+handleRoutes(express);
