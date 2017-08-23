a8750771ad7c9ae113e7acd3255a099018ddf005
asarnaout <asarnaout@hotmail.com>
2017-08-23 12:50:27 +0200

    Adding the beer style in the details page

--- src/Views/Details/Details.js
+++ src/Views/Details/Details.js
@@ -12,9 +12,19 @@ class Details extends Component {
     constructor(props){
         super(props);
         this.searchHandler = this.searchHandler.bind(this);
-        this.state ={
-            beer:{}
-        };
+        this.state = {
+            organic: '',
+            thumbnail: '',
+            available: '',
+            glass: '',
+            srm: '',
+            abv:'',
+            ibu: '',
+            name: '',
+            description:'',
+            statusDisplay: '',
+            createDate:''
+        }
     }
 
     componentWillMount(){
@@ -23,25 +33,34 @@ class Details extends Component {
             url: config.apiEndpoint + 'beer?id=' + this.props.match.params.beerId,
           })).then(response => {
               let beer = response.data.data;
-              this.setState({beer: beer});
-          }); 
+              this.setState({
+                  name: beer.name,
+                  description: this.isProvided(beer.description)? beer.description : 'Description Not Available',
+                  statusDisplay: this.isProvided(beer.statusDisplay)? beer.statusDisplay:'Verification Status Not Available',
+                  createDate: this.isProvided(beer.createDate)? beer.createDate : 'Creation Date Not Available',
+                  organic: beer.isOrganic === "N"? "No" : "Yes",
+                  thumbnail: this.isProvided(beer.labels)? beer.labels.large : config.defaultDashboardIconCdn,
+                  available: this.isProvided(beer.available)? beer.available.description : 'Not Available',
+                  glass: this.isProvided(beer.glass)? beer.glass.name : 'No Glassware Available',
+                  srm: this.isProvided(beer.srm)? '#' + beer.srm.hex : 'No SRM Code Available',
+                  abv: this.isProvided(beer.abv)? beer.abv :  'ABV Not Available',
+                  ibu: this.isProvided(beer.ibu)? beer.ibu :  'IBU Not Available',
+                  style: this.isProvided(beer.style)? beer.style.name : 'Beer Style Not Available'
+              });
+        }); 
     }
 
     searchHandler(){
         
     }
 
-    render(){
-        let beer = this.state.beer;
-        let thumbnail = beer.labels === undefined? config.defaultDashboardIconCdn : beer.labels.large;
-        let organic = beer.isOrganic === "N"? "No" : "Yes";
-        let availability = beer.available === undefined? 'Not Available' : beer.available.description;
-        let glass = beer.glass === undefined? 'No Glassware Available' : beer.glass.name;
-        let srm = beer.srm === undefined? 'No SRM Code Provided' : '#' + beer.srm.hex;
-        
+    isProvided(value) {
+        return value !== undefined && value != '' && value != null;
+    }
+
+    render(){                
         return (
                 <div id="DetailsPage">
-
                     <div className="turquoise-bg">
                         <div className="container">
                             <Filter searchHandler={this.searchHandler}/>
@@ -51,21 +70,23 @@ class Details extends Component {
                         <div className="h20"/>
                         <div>
                                 <div className ="col-sm-7 col-xs-12">
-                                    <span className="fs-40 bold">{beer.name}</span>
+                                    <span className="fs-40 bold">{this.state.name}</span>
                                     <div className="h10"/>
-                                    <p className="fs-20 justify-text"><span className="bold">Description:</span> {beer.description}</p>
-                                    <p className="fs-20"><span className="bold">Verification Status:</span> {beer.statusDisplay}</p>
-                                    <p className="fs-20"><span className="bold">ABV:</span> {beer.abv}</p>
-                                    <p className="fs-20"><span className="bold">IBU:</span> {beer.ibu}</p>
-                                    <p className="fs-20"><span className="bold">Organic:</span> {organic}</p>
-                                    <p className="fs-20"><span className="bold">Availability:</span> {availability}</p>
-                                    <p className="fs-20"><span className="bold">Glassware:</span> {glass}</p>
-                                    <p className="fs-20"><span className="bold">SRM:</span> {srm}</p>
-                                    <p className="fs-20"><span className="bold">Created:</span> {beer.createDate}</p>
+                                    <span className="fs-18"><i>{this.state.style}</i></span>
+                                    <div className="h10"/>
+                                    <p className="fs-20 justify-text"><span className="bold">Description:</span> {this.state.description}</p>
+                                    <p className="fs-20"><span className="bold">Verification Status:</span> {this.state.statusDisplay}</p>
+                                    <p className="fs-20"><span className="bold">ABV:</span> {this.state.abv}</p>
+                                    <p className="fs-20"><span className="bold">IBU:</span> {this.state.ibu}</p>
+                                    <p className="fs-20"><span className="bold">Organic:</span> {this.state.organic}</p>
+                                    <p className="fs-20"><span className="bold">Availability:</span> {this.state.available}</p>
+                                    <p className="fs-20"><span className="bold">Glassware:</span> {this.state.glass}</p>
+                                    <p className="fs-20"><span className="bold">SRM:</span> {this.state.srm}</p>
+                                    <p className="fs-20"><span className="bold">Created:</span> {this.state.createDate}</p>
                                 </div>
 
                                 <div className ="col-sm-5 col-xs-12">
-                                    <img src={thumbnail} className="center-item full-width"/>
+                                    <img src={this.state.thumbnail} className="center-item full-width"/>
                                 </div>
                                 
                         </div>
