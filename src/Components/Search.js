587bdf7b1e89cb4fbbbeaa98eaa1c6b4c1063106
asarnaout <asarnaout@hotmail.com>
2017-08-27 19:51:22 +0200

    Returining the back button handler

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -13,6 +13,7 @@ class Search extends Component {
         super(props);
         this.nextPage = this.nextPage.bind(this);
         this.previousPage = this.previousPage.bind(this);
+        this.onBackButtonEvent = this.onBackButtonEvent.bind(this);
         window.onpopstate = this.onBackButtonEvent;
     }
 
@@ -25,6 +26,11 @@ class Search extends Component {
         } else {
             document.body.style.backgroundImage = "none";
         }
+    }
+
+    onBackButtonEvent(e){
+        let qs = queryString.parse(this.props.location.search);
+        this.props.setFilter(qs);
     }
 
     async nextPage() {
