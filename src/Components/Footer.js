51ee82817f5b1bba2ffc1d7cc771f4deff484d6b
asarnaout <asarnaout@hotmail.com>
2017-08-31 17:59:16 +0200

    Hiding the background image in the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,43 +6,45 @@ class Footer extends Component {
 
     render() {
         return (
-            <div className="footer fs-14">
-                <div className="container">
-                    <div className="row no-margin center-text">
+            <div className="footer fs-14 white-bg">
+                <div>
+                    <div className="container">
+                        <div className="row no-margin center-text">
 
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <Link to={'/aboutus'}>
-                                <span className="white-text">About Us</span>
-                            </Link>
-                        </div>
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <Link to={'/aboutus'}>
+                                    <span className="white-text">About Us</span>
+                                </Link>
+                            </div>
 
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <Link to={'/contactus'}>
-                                <span className="white-text">Contact Us</span>
-                            </Link>
-                        </div>
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <Link to={'/contactus'}>
+                                    <span className="white-text">Contact Us</span>
+                                </Link>
+                            </div>
 
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <Link to={'/privacypolicy'}>
-                                <span className="white-text">Privacy Policy</span>
-                            </Link>
-                        </div>
-                        
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <Link to={'/termsofservice'}>
-                                <span className="white-text">Terms Of Service</span>
-                            </Link>
-                        </div>
-                        
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <Link to={'/cookiepolicy'}>
-                                <span className="white-text">Cookie Policy</span>
-                            </Link>
-                        </div>
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <Link to={'/privacypolicy'}>
+                                    <span className="white-text">Privacy Policy</span>
+                                </Link>
+                            </div>
+                            
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <Link to={'/termsofservice'}>
+                                    <span className="white-text">Terms Of Service</span>
+                                </Link>
+                            </div>
+                            
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <Link to={'/cookiepolicy'}>
+                                    <span className="white-text">Cookie Policy</span>
+                                </Link>
+                            </div>
 
-                        <div className="col-xs-12 col-sm-2 footer-item">
-                            <img id="BeerIcon" src={BeerIconImage} width="20" alt="beer_ico" />
-                            <span className="white-text tm">&nbsp;Beeraholic™</span>
+                            <div className="col-xs-12 col-sm-2 footer-item">
+                                <img id="BeerIcon" src={BeerIconImage} width="20" alt="beer_ico" />
+                                <span className="white-text tm">&nbsp;Beeraholic™</span>
+                            </div>
                         </div>
                     </div>
                 </div>
